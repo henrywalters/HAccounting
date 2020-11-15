7cdd6081bf332b73cd75631526505b66ae1b0fa8
Henry Walters <henrywalters20@gmail.com>
2020-11-15 01:01:26 -0500

    A bit of work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, RequestMethod} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import {getConnection} from "typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  async configure(consumer: MiddlewareConsumer) {
+      await this.setEntityConnections();
+  }
+
+  async setEntityConnections() {
+      const connection = await getConnection();
+      connection.entityMetadatas.forEach(entity => {
+          (entity.target as any).useConnection(connection);
+      })
+  }
+}
